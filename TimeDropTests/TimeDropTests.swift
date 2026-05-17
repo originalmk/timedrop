f1706901e4cf011cadac3c5aa67ca5d3c66d6870
Maciej Krzyżanowski <krzmaciek@gmail.com>
2026-05-17 19:25:39 +0200

    Initial Commit

---
+++ TimeDropTests/TimeDropTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeDropTests.swift
+//  TimeDropTests
+//
+//  Created by Maciej Krzyżanowski on 17/05/2026.
+//
+
+import Testing
+@testable import TimeDrop
+
+struct TimeDropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
